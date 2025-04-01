621684b593b13811f676f0cc551e304ad69f6c34
Lamassu15 <bazo.assyr@gmail.com>
2025-04-01 09:54:47 +0200

    UI bugs

--- my-project/src/components/Members.tsx
+++ my-project/src/components/Members.tsx
@@ -7,20 +7,20 @@ function Members() {
         {MEMBERS.map((members, index) => (
           <div
             key={index}
-            className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg"
+            className="bg-gray-800 p-6 rounded-lg shadow-lg"
           >
             <img
               className="w-40 h-32 rounded mx-auto mb-4 transition-all duration-300 hover:scale-150 object-cover"
               src={members.IMG}
               alt={`${members.NAME} Avatar`}
             />
-            <h3 className="text-xl font-bold text-gray-900 dark:text-white">
+            <h3 className="text-xl font-bold text-fonts">
               {members.NAME}
             </h3>
-            <p className="text-gray-600 dark:text-gray-400">
+            <p className="text-gray-400">
               Experience: {members.EXPERIENCE}
             </p>
-            <p className="text-gray-600 dark:text-gray-400">{members.BIO}</p>
+            <p className="text-gray-400">{members.BIO}</p>
           </div>
         ))}
       </div>
